4ac6be8a1f6262cddcd790b96f324f667eaf558f
Stacy Zhao <stacyzhao@hotmail.com>
2016-03-30 20:31:19 -0400

    remove special character and main

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,11 @@
+import re
 # checks if word is a palindrome. returns true or false
+def remove_special_character(text):
+    # user_input = input ("enter some words to check if its a palindrome: ")
+    clean_text = re.sub("[^A-Za-z]+", "", text)
+    clean_text = clean_text.lower()
+    return clean_text
+
 def is_palindrome(text):
     if len(text) == 0 or len(text) == 1:
         return False
@@ -11,3 +18,15 @@ def is_palindrome(text):
         if text[0] == text[-1]:
             return is_palindrome(text[1:-1])
         return False
+
+def main():
+    user_input = input("enter some words to check if its a palindrome: ")
+    clean_text = remove_special_character(user_input)
+    print(clean_text)
+    if is_palindrome(clean_text):
+        print ("palindrome is true")
+    else:
+        print ("palindrome is false")
+
+if __name__ == '__main__':
+    main()
